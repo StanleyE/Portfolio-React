c67ab07d4253424199434d565e99d72f8f93af70
Erica <Erica.Stanley@live.ca>
2019-03-19 10:59:06 -0400

    adding more to rough draft

--- src/App.js
+++ src/App.js
@@ -20,8 +20,14 @@ class App extends Component {
           <p>Also need to make it have a daytime and nighttime css that is based of user time zone but also has a button on top corner tha they can manually adjust</p>
           <About />
           <Skills />
-          <ProjectBoard />
+          <p>With these skills I can help you make your website how you want it</p>
+          {/* moving dives to create a fake website */}
+          <p>Sofisticated or Energetic</p>
+          {/* That starts white with light hints of color that scrolls to bold colors */}
+          <p>Taking any quirks that make you... you! and adding them to your website to make it unique</p>
+          {/* "Unique" will be further down and spider will swing down and give a heart emoji */}
           <h5>Along came a Spider?</h5>
+          <ProjectBoard />
           <Contact />
         </header>
       </div>
