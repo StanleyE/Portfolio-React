b5dc94fae28e4184d2629a325d74cf0db1005c84
Erica <Erica.Stanley@live.ca>
2019-03-20 11:15:31 -0400

    adding react-bootstrap

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-
-    }
 
     render() {
         return (
             <div className="aboutMain">
                 <h2>About me with image overlay</h2>
+                <div className="profImage">
+                    <h2>Professional Img</h2>
+                </div>
             </div>
         );
     }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+// import Button from 'react-bootstrap/Button';
 
 class Contact extends Component {
     render() {
         return (
             <div className="callMeMaybe">
                 <h4>So call me maybe?</h4>
+                {/* <Button variant="outline-primary">Call me maybe</Button> */}
             </div>
         );
     }
