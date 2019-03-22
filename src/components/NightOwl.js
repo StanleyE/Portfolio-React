25dea9cf4b48d0311abb8b10f689b6b88f2be39f
Erica <Erica.Stanley@live.ca>
2019-03-22 14:08:47 -0400

    added NighOwl draft

---
+++ src/components/NightOwl.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NightOwl extends Component {
+    render() {
+        return (
+            <div className = "nightOwl">
+                <p>When Clicked will turn site to night mode with Black background and white text</p>
+            </div>
+        );
+    }
+}
+
+export default NightOwl;
