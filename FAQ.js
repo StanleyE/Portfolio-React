52c7361da5fb87f9b3b556e18cfb4afc21fd3691
Erica <Erica.Stanley@live.ca>
2019-03-22 15:43:12 -0400

    adding more rough content

---
+++ FAQ.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FAQ extends Component {
+    render() {
+        return (
+            <div>
+                <p>"Frequently asked questions" where I give cheeky answers</p>
+                <p>example: Q: Can you intergrate Google Ads into a site?</p>
+                <p>Anwser: Just a button names Click Bait that leads to a component that is heavy dosed with Ads and lorem ipsum content</p>
+            </div>
+        );
+    }
+}
+
+export default FAQ;
