04806b8382a4ac27ca1ce67fef74b5cb1b496ce5
Erica <Erica.Stanley@live.ca>
2019-03-25 11:13:40 -0400

    added more shell components

---
+++ src/components/MyStory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyStory extends Component {
+    render() {
+        return (
+            <div>
+                <p>Sezure Fish!</p>
+            </div>
+        );
+    }
+}
+
+export default MyStory;
